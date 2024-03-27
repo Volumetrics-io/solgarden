e8ec0ea8c719032d8fa387385812417c00a9e953
Laurent Baumann <git@lobau.io>
2024-03-26 17:46:21 -0700

    whyyy

--- mr-board.js
+++ mr-board.js
@@ -2,42 +2,27 @@ class MRBoard extends MREntity {
 
     constructor() {
         super()
-
-        //// DEBUG
-        const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.25);
-        const material = new THREE.MeshPhongMaterial({
-            color: '#0235ff',
-            side: 2,
-            transparent: true,
-            opacity: 0.2,
-            specular: '#7989c4',
-            clipping: true
-        })
-
-        this.mesh = new THREE.Mesh(geometry, material)
-        this.object3D.add(this.mesh)
-        //// DEBUG
     }
 
     connectedCallback() {
-        this.generateGridElements(8, 8);
-    }
-
-    generateGridElements(n, m) {
         const models = ["/tiles/0.glb", "/tiles/1.glb", "/tiles/2.glb"];
         const rotations = [0, 90, 180, 270];
         const scale = 0.1;
+        const rows = 8;
+        const columns = 8;
 
-        for (let x = 0; x < n; x++) {
-            for (let y = 0; y < m; y++) {
-                const offsetX = x * scale;
-                const offsetY = y * scale;
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < columns; c++) {
+                let offsetRow = r * scale;
+                let offsetCol = c * scale;
+                let randomModel = models[Math.floor(Math.random() * models.length)];
+                let randomRotation = rotations[Math.floor(Math.random() * rotations.length)];
 
                 let tile = document.createElement("mr-tile");
-                tile.dataset.model = models[Math.floor(Math.random() * models.length)];
+                tile.dataset.model = randomModel;
                 tile.dataset.scale = scale;
-                tile.dataset.rotation = "0 0 " + rotations[Math.floor(Math.random() * rotations.length)];
-                tile.dataset.position = `${offsetX} 0 ${offsetY}`;
+                tile.dataset.rotation = `0 0 ${randomRotation}`;
+                tile.dataset.position = `${offsetRow} 0 ${offsetCol}`;
                 this.append(tile);
             }
         }
