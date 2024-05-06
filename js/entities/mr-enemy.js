ce574143a6755cb4c726bfa6768832444c1c1051
Laurent Baumann <git@lobau.io>
2024-05-06 10:04:36 -0700

    Clean up

--- js/entities/mr-enemy.js
+++ js/entities/mr-enemy.js
@@ -4,16 +4,12 @@ class MREnemy extends MREntity {
         super()
 
         this.el = document.createElement("mr-model");
-        this.damageContainer = document.createElement("mr-div")
-        this.damageValue = document.createElement("mr-text");
-        this.damageValueBackface = document.createElement("mr-text");
 
         this.swooshSound = document.createElement("mr-entity");
         this.bowReleaseSound = document.createElement("mr-entity");
         this.critSound = document.createElement("mr-entity");
 
         this.poof = document.createElement("mr-model");
-
     }
 
     connected() {
@@ -45,31 +41,6 @@ class MREnemy extends MREntity {
             loop: false,
             action: "stop"
         })
-
-
-        // Damage display
-        this.appendChild(this.damageContainer);
-        this.damageContainer.dataset.position = '0 2.5 0';
-        this.damageContainer.style.scale = 1 / 0.05; // will be fixed soon in MRjs
-        Object.assign(this.damageContainer.style, {
-            padding: "5px 10px",
-            width: "auto",
-            borderRadius: "5px",
-            backgroundColor: Colors.health,
-            visibility: 'hidden'
-        });
-
-        this.damageContainer.appendChild(this.damageValue);
-        this.damageValue.style.fontSize = '16px';
-        this.damageValue.style.color = 'white';
-        this.damageValue.textObj.position.setX((-this.damageValue.width / 2) / 0.005);
-
-        this.damageContainer.appendChild(this.damageValueBackface);
-        this.damageValueBackface.style.fontSize = '16px';
-        this.damageValueBackface.style.color = 'white';
-        this.damageValueBackface.textObj.position.setX((-this.damageValueBackface.width / 2) / 0.005);
-        this.damageValueBackface.dataset.rotation = "0 180 0";
-        this.damageValueBackface.dataset.position = "0 0 -0.001";
 
         // Poof effect
         this.appendChild(this.poof);
@@ -103,19 +74,6 @@ class MREnemy extends MREntity {
         this.bowReleaseSound.components.set('audio', {
             action: "play"
         })
-    }
-
-    showDamage(string) {
-        this.damageValue.innerText = string;
-        this.damageValueBackface.innerText = string;
-
-        this.damageContainer.style.visibility = "visible";
-
-        setTimeout(event => {
-            this.damageValue.innerText = "";
-            this.damageValueBackface.innerText = "";
-            this.damageContainer.style.visibility = "hidden";
-        }, 500);
     }
 }
 
--- js/entities/mr-player.js
+++ js/entities/mr-player.js
@@ -8,10 +8,6 @@ class MRPlayer extends MREntity {
 
         this.swooshSound = document.createElement("mr-entity");
         this.bowReleaseSound = document.createElement("mr-entity");
-
-        this.damageContainer = document.createElement("mr-div")
-        this.damageValue = document.createElement("mr-text");
-        this.damageValueBackface = document.createElement("mr-text");
     }
 
     connected() {
@@ -48,29 +44,6 @@ class MRPlayer extends MREntity {
         this.light.setAttribute('color', "#ffffff");
         this.light.setAttribute('intensity', 0.03);
         this.light.dataset.position = `0 0.6 0.3`;
-
-        this.appendChild(this.damageContainer);
-        this.damageContainer.dataset.position = '0 2.5 0';
-        this.damageContainer.style.scale = 1 / 0.05; // will be fixed soon in MRjs
-        Object.assign(this.damageContainer.style, {
-            padding: "5px 10px",
-            width: "auto",
-            borderRadius: "5px",
-            backgroundColor: '#e72d75',
-            visibility: 'hidden'
-        });
-
-        this.damageContainer.appendChild(this.damageValue);
-        this.damageValue.style.fontSize = '16px';
-        this.damageValue.style.color = 'white';
-        this.damageValue.textObj.position.setX((-this.damageValue.width / 2) / 0.005);
-
-        this.damageContainer.appendChild(this.damageValueBackface);
-        this.damageValueBackface.style.fontSize = '16px';
-        this.damageValueBackface.style.color = 'white';
-        this.damageValueBackface.textObj.position.setX((-this.damageValueBackface.width / 2) / 0.005);
-        this.damageValueBackface.dataset.rotation = "0 180 0";
-        this.damageValueBackface.dataset.position = "0 0 -0.001";
     }
 
     playIdleAnimation() {
@@ -119,19 +92,6 @@ class MRPlayer extends MREntity {
                 }
             })
         }, 500)
-    }
-
-    showDamage(string) {
-        this.damageValue.innerText = string;
-        this.damageValueBackface.innerText = string;
-
-        this.damageContainer.style.visibility = "visible";
-
-        setTimeout(event => {
-            this.damageValue.innerText = "";
-            this.damageValueBackface.innerText = "";
-            this.damageContainer.style.visibility = "hidden";
-        }, 500);
     }
 
 }
