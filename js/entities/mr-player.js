5712eda26dadf0ef62dd3c8b06b365c08947f0f8
Hannah Bollar <github@hannahbollar.com>
2024-05-08 22:09:41 -0500

    update `mr-player` for clip specific animations by name (#35)
    Signed-off-by: hanbollar <github@hannahbollar.com>

--- js/entities/mr-player.js
+++ js/entities/mr-player.js
@@ -4,46 +4,21 @@ class MRPlayer extends MREntity {
         super()
 
         this.el = document.createElement("mr-model");
+        this.el.src = "assets/models/dot-test.glb";
+        this.el.style.pointerEvents = 'none';
+
         this.light = document.createElement("mr-light");
 
         this.swooshSound = document.createElement("mr-entity");
         this.bowReleaseSound = document.createElement("mr-entity");
 
-        // this.el.onLoad = () => {
-        //     // let mixer = this.el.mixer;
-        //     let animations = this.el.animations;
-        //
-        //     this.el.mixer = new THREE.AnimationMixer(this.el.object3D);
-        //     this.el.mixer.clipAction(THREE.AnimationUtils.subclip(animations[0], 'idle', 0, 30)).setDuration(1).play(); //0
-        //     this.el.mixer.clipAction(THREE.AnimationUtils.subclip(animations[0], 'run', 60, 75)).setDuration(1).play(); //1
-        //     this.el.mixer._actions[0].enabled = true;
-        //     this.el.mixer._actions[1].enabled = false;
-        //
-        //     console.log(this.el.mixer, animations);
-        //
-        //     this.playIdleAnimation();
-        // }
+        this.el.onLoad = () => {
+            this.playIdleAnimation();
+        }
     }
 
     connected() {
         this.appendChild(this.el);
-        this.el.src = "assets/models/dot-test.glb";
-        this.el.style.pointerEvents = 'none';
-
-        //character
-        // const gltfLoader = new GLTFLoader();
-        // gltfLoader.load('./character.glb', function(gltf) {
-        //     character = gltf.scene;
-        //     mixer = new THREE.AnimationMixer(character);
-        //     mixer.clipAction(THREE.AnimationUtils.subclip(gltf.animations[0], 'idle', 0, 221)).setDuration(6).play(); //0
-        //     mixer.clipAction(THREE.AnimationUtils.subclip(gltf.animations[0], 'run', 222, 244)).setDuration(0.7).play(); //1
-        //     mixer._actions[0].enabled = true;
-        //     mixer._actions[1].enabled = false;
-        //     character.children[1].visible = true;
-        //     character.children[2].visible = false;
-        //     scene.add(character);
-        // }); //end loader
-
         this.appendChild(this.swooshSound);
         this.appendChild(this.bowReleaseSound);
 
@@ -66,22 +41,19 @@ class MRPlayer extends MREntity {
     }
 
     playIdleAnimation() {
-        let action = this.el.mixer.clipAction(0);
-        action.reset.play();
-
-        // this.el.components.set("animation", {
-        //     clip: 0,
-        //     action: "play",
-        //     loop: true,
-        // });
+        this.el.components.set("animation", {
+            clip: 'idle',
+            action: "play",
+            loop: true,
+        });
     }
 
     playCombatAnimation() {
         this.el.components.set("animation", {
-            clip: 2,
+            clip: 'attack-melee',
             action: "play",
             loop: false,
-            // clampWhenFinished: true
+            clampWhenFinished: true
         });
     }
 
