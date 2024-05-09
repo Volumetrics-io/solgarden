7120f9d6448904796891fe45a5b10c50c39afa8c
Laurent Baumann <git@lobau.io>
2024-05-08 17:02:54 -0700

    add shack and solar in spawn

---
+++ js/entities/mr-spawn.js
@@ -0,0 +1,35 @@
+/////////////////////
+// I
+class MRSpawnI extends MREntity {
+
+    constructor() {
+        super()
+        this.el = document.createElement("mr-model");
+    }
+
+    connected() {
+        this.appendChild(this.el);
+        this.el.src = "assets/biomes/spawn/title-solar.glb";
+        this.el.style.pointerEvents = 'none';
+        this.el.dataset.rotation = '0 270 0';
+    }
+}
+customElements.define('mr-spawn-i', MRSpawnI);
+
+/////////////////////
+// II
+class MRSpawnII extends MREntity {
+
+    constructor() {
+        super()
+        this.el = document.createElement("mr-model");
+    }
+
+    connected() {
+        this.appendChild(this.el);
+        this.el.src = "assets/biomes/spawn/title-shack.glb";
+        this.el.style.pointerEvents = 'none';
+        this.el.dataset.rotation = '0 270 0';
+    }
+}
+customElements.define('mr-spawn-ii', MRSpawnII);
