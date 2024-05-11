ceca4dc8438068ade0b470731e34fafa75a1b342
Hannah Bollar <github@hannahbollar.com>
2024-05-10 20:05:47 -0500

    fix so `idle` and `attack-melee` are using the right subclip frames (#44)
    Signed-off-by: hanbollar <github@hannahbollar.com>

--- js/entities/mr-player.js
+++ js/entities/mr-player.js
@@ -13,6 +13,41 @@ class MRPlayer extends MREntity {
         this.bowReleaseSound = document.createElement("mr-entity");
 
         this.el.onLoad = () => {
+            console.log(this.el.animations);
+
+            /* --- Cleanup animation clips for player --- */
+
+            // Since the animations we're using take up all the frames,
+            // we want to skip the frames that we know are not needed.
+
+            const _updateClipsFor = (name, startFrame, endFrame) => {
+                // Find the original clip
+                const index = this.el.animations.findIndex((clip) => clip.name === name);
+                if (index === -1) {
+                    console.warn('Clip not found by name:', name);
+                    return;
+                }
+                const originalClip = this.el.animations[index];
+
+                // Assume 24 frames per second, adjust according to your animation data
+                const fps = 24;
+                const newClip = THREE.AnimationUtils.subclip(
+                    originalClip,
+                    originalClip.name,
+                    startFrame,
+                    endFrame,
+                    fps
+                );
+
+                // Replace the original clip with the new subclip in the animations array
+                this.el.animations[index] = newClip;
+            }
+
+            _updateClipsFor('idle', 1, 60);
+            _updateClipsFor('attack-melee', 61, 75);
+
+            /* --- Play the default animation --- */
+
             this.playIdleAnimation();
         }
     }
