b0ae320e20608667cfb98e381b0dcc3c1df6179a
hanbollar <github@hannahbollar.com>
2024-05-16 15:24:01 -0700

    update for debug
    Signed-off-by: hanbollar <github@hannahbollar.com>

--- js/utils.js
+++ js/utils.js
@@ -91,7 +91,7 @@ function updateClipsFor(name, startFrame, endFrame) {
     console.log('within updateClipsFor, this is:', this);
 
     // Find the original clip
-    const index = this.el.animations.findIndex((clip) => clip.name === name);
+    const index = this.animations.findIndex((clip) => clip.name === name);
     if (index === -1) {
         console.warn('Clip not found by name:', name);
         return;
