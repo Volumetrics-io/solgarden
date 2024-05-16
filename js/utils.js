7f4c64adde8008d9612d62e0a1e870202ff51d84
hanbollar <github@hannahbollar.com>
2024-05-16 13:50:55 -0700

    missed one
    Signed-off-by: hanbollar <github@hannahbollar.com>

--- js/utils.js
+++ js/utils.js
@@ -90,7 +90,7 @@ const Animator = {
 const updateClipsFor = (name, startFrame, endFrame) => {
 
     // Find the original clip
-    const index = this.el.animations.findIndex((clip) => clip.name === name);
+    const index = this.animations.findIndex((clip) => clip.name === name);
     if (index === -1) {
         console.warn('Clip not found by name:', name);
         return;
